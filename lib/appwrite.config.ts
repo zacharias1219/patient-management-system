fb7684416c301494861f03bd883e023608eea3a2
Richard Abishai <rickoshade1891@gmail.com>
2024-08-10 22:57:19 +0530

    chore: Remove deprecated ThemeProvider component and add new StatusBadge and StatCard components

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,7 +1,14 @@
-import * as sdk from 'node-appwrite';
+import * as sdk from "node-appwrite";
 
 export const {
-    PROJECT_ID, API_KEY, DATABASE_ID, PATIENT_CALLECTION_ID, DOCTOR_COLLECTION_ID , APPOINTMENT_COLLECTION_ID, NEXT_PUBLIC_BUCKET_ID: BUCKET_ID, NEXT_PUBLIC_ENDPOINT: ENDPOINT
+  NEXT_PUBLIC_ENDPOINT: ENDPOINT,
+  PROJECT_ID,
+  API_KEY,
+  DATABASE_ID,
+  PATIENT_COLLECTION_ID,
+  DOCTOR_COLLECTION_ID,
+  APPOINTMENT_COLLECTION_ID,
+  NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
 } = process.env;
 
 const client = new sdk.Client();
@@ -9,6 +16,6 @@ const client = new sdk.Client();
 client.setEndpoint(ENDPOINT!).setProject(PROJECT_ID!).setKey(API_KEY!);
 
 export const databases = new sdk.Databases(client);
-export const storage = new sdk.Storage(client);
-export const messaging = new sdk.Messaging(client);
 export const users = new sdk.Users(client);
+export const messaging = new sdk.Messaging(client);
+export const storage = new sdk.Storage(client);
